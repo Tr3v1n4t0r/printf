1cd176f76052827a2aa525ab165a16ff68b8dfb8
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-28 21:04:57 +0000

    Helper Function section 2

--- Helper_Functions2.c
+++ Helper_Functions2.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 
-
 /**
  * _putchar - writes the character c to stdout.
  * @c: The character to print.
@@ -11,4 +10,46 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+/**
+ * *_strchr - function to count the character..
+ * @s: is the string 1.
+ * @c: is the string 2.
+ * Return: - s and counter.
+ */
+char *_strchr(char *s, char c)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		if (s[counter] == c)
+		{
+			return (s + counter);
+		}
+	}
+	if (s[counter] == c)
+	{
+		return (s + counter);
+	}
+	return ('\0');
+}
+
+/**
+ * _power - functions to multiple base to the power.
+ * @base: is the base number going into it.
+ * @exponent: the exponent for the base power number.
+ * Return: - this returns the result.
+ */
+
+int _power(int base, int exponent)
+{
+	int result = 1;
+
+	for (exponent; exponent > 0; exponent--)
+	{
+		result = result * base;
+	}
+	return (result);
 }
