151493bf3627410e6fb3bfc979e4fdbcd75cb8e6
Kenfernandezjr <Kenfernandezjr@github.com>
2019-10-29 23:33:42 +0000

    added rev_string to helper functions

--- Helper_Functions2.c
+++ Helper_Functions2.c
@@ -69,3 +69,32 @@ int _len(int num)
 	}
 	return (count);
 }
+
+/**
+ * rev_string - function that reserves a string.
+ * @s: str held the word Holberton and putting it in reverse.
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	char *x = s;
+	char y;
+	int counter;
+
+	counter = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		counter++;
+	}
+	s--;
+	for (counter /= 2; counter > 0; counter--)
+	{
+		y = *s;
+		*s = *x;
+		*x = y;
+		x++;
+		s--;
+	}
+}
