a3cf3fbf62aa3bd2445b3040058029a58422a2f7
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-29 19:09:42 -0700

    added case for negative

--- Helper_Functions.c
+++ Helper_Functions.c
@@ -7,25 +7,33 @@
  * @base: base.
  * Return: the string
  */
-char *_itoa(unsigned int value, char *buffer, int base)
+char *_itoa(int value, char *buffer, int base)
 {
-
+	int n = _abs(value);
 	int i = 0;
 	int r;
 
-	while (value)
+	while (n)
 	{
-		r = value % base;
+		r = n % base;
 		if (r >= 10)
 			buffer[i++] = 65 + (r - 10);
 		else
 			buffer[i++] = 48 + r;
-		value = value / base;
+		n = n / base;
 	}
 	if (i == 0)
 		buffer[i++] = '0';
+	if (value < 0 && base == 10)
+		buffer[i++] = '-';
 
 	buffer[i] = '\0';
 
 	return (rev_string(buffer, 0, i - 1));
+}
+
+unsigned int _abs(int n)
+{
+	int const mask = n >> (sizeof(int) * 8 - 1);
+	return ((n + mask) ^ mask);
 }
