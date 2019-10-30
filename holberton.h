66902c1f36fae2f750bd7556a6e8982474e09e45
Tr3v1n4t0r <813@holbertonschool.com>
2019-10-29 18:28:16 -0700

    finally fixed the problem

--- holberton.h
+++ holberton.h
@@ -8,11 +8,6 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-int _strlen(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 int _power(int base, int exponent);
 int _len(int num);
@@ -20,6 +15,6 @@ int type_c(char);
 int type_s(char *);
 int type_i(int);
 char *rev_string(char *, int, int);
-char* _itoa(unsigned int value, char* buffer, int base)
+char *_itoa(unsigned int, char *, int);
 
 #endif
